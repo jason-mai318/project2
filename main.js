741a09d977e959ab7ca98bcc5274fa07ef6517f9
Jason Mai <jasonmai318@gmail.com>
2019-06-23 19:25:33 -0400

    added accordion and carousel layouts

--- main.js
+++ main.js
@@ -38,4 +38,40 @@ function runIntro(name) {
   welcome_text.style.animationFillMode = "forwards";
   welcome_text.innerText = "Hello" + name + "!";
   document.getElementById("display-4").innerHTML = "Welcome" + name + "!";
+  setTimeout(function() {
+    document.body.style.overflow = "auto";
+  }, 4000);
+}
+//carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+setTimeout(function() {
+  document.body.style.overflow = "auto";
+}, 4000);
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
